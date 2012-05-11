28ae8e86bd7eff70eaafb1f4ce3767835e150528
Ryan Cole <ryan@rycole.com>
2012-05-11 11:41:07 -0500

    finished node removal

--- src/tree.js
+++ src/tree.js
@@ -81,9 +81,124 @@ Tree.prototype.remove_node = function (node) {
     
     if (node.left != null || node.right != null) {
         
+        var balance_node = null;
+        var replace_node = null;
         
+        if (node.left != null) {
+            
+            replace_node = this.get_max_node(node.left);
+            
+            if (replace_node != node.left) {
+                
+                replace_node.parent.right = replace_node.left;
+                
+                if (replace_node.left)
+                    replace_node.left.parent = replace_node.parent;
+                
+                replace_node.left = node.left;
+                replace_node.left.parent = replace_node;
+                
+                balance_node = replace_node.parent;
+                
+            }
+            
+            replace_node.parent = node.parent;
+            replace_node.right = node.right;
+            
+            if (replace_node.right)
+                replace_node.right.parent = replace_node;
+            
+            if (node == this.max_node)
+                this.max_node = replace_node;
+            
+            // reduce count
+            this.count--;
+            
+        } else {
+            
+            replace_node = this.get_min_node(node.right);
+            
+            if (replace_node != node.right) {
+                
+                replace_node.parent.left = replace_node.right;
+                
+                if (replace_node.right)
+                    replace_node.right.parent = replace_node.parent;
+                
+                replace_node.right = node.right;
+                replace_node.right.parent = replace_node;
+                
+                balance_node = replace_node.parent;
+                
+            }
+            
+            replace_node.parent = node.parent;
+            replace_node.left = node.left;
+            
+            if (replace_node.left)
+                replace_node.left.parent = replace_node;
+            
+            if (node == this.min_node)
+                this.min_node = replace_node;
+            
+            // reduce count
+            this.count--;
+            
+        }
+        
+        // update parent of node being removed
+        if (node.is_left_child)
+            node.parent.left = replace_node;
+            
+        else if (node.is_right_child)
+            node.parent.right = replace_node;
+            
+        else
+            this.root = replace_node;
+        
+        // balance the tree
+        this.balance(balance_node ? balance_node : replace_node);
+        
+    } else {
+        
+        // if node is leaf, remove it and balance from its parent
+        if (node.is_left_child) {
+            
+            node.parent.left = null;
+            this.count--;
+            
+            if (node == this.min_node)
+                this.min_node = node.parent;
+            
+            this.balance(node.parent);
+            
+        } else if (node.is_right_child) {
+            
+            node.parent.right = null;
+            this.count--;
+            
+            if (node == this.max_node)
+                this.max_node = node.parent;
+            
+            this.balance(node.parent);
+            
+        } else {
+            
+            this.clear();
+            
+        }
         
     }
+    
+};
+
+// clear
+Tree.prototype.clear = function () {
+    
+    this.root = null;
+    this.min_node = null;
+    this.max_node = null;
+    this.count = 0;
     
 };
 
@@ -284,6 +399,62 @@ Tree.prototype.right_rotate = function (node) {
     
     left.right = node;
     node.parent = left;
+    
+};
+
+// get min node
+Tree.prototype.get_min_node = function (node) {
+    
+    if (!node)
+        return this.min_node;
+    
+    var min_node = node;
+    
+    this.traverse(function (node) {
+        
+        var result = null;
+        
+        if (node.left) {
+            
+            min_node = node.left;
+            result = node.left;
+            
+        }
+        
+        // null stops the traversal
+        return result;
+        
+    }, node);
+    
+    return min_node;
+    
+};
+
+// get max node
+Tree.prototype.get_max_node = function (node) {
+    
+    if (!node)
+        return this.max_node;
+    
+    var max_node = node;
+    
+    this.traverse(function (node) {
+        
+        var result = null;
+        
+        if (node.right) {
+            
+            max_node = node.right;
+            result = node.right;
+            
+        }
+        
+        // null stops the traversal
+        return result;
+        
+    }, node);
+    
+    return max_node;
     
 };
 
